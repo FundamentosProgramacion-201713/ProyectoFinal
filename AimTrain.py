f1a56685796d597a6e51fb712f015d3902e3b36b
Sebastian <sebfut@hotmail.com>
2017-11-07 23:02:57 -0600

    Esqueleto de pygame

---
+++ AimTrain.py
@@ -0,0 +1,48 @@
+#encoding: UTF-8
+#Sebastian Morales Martin
+#Juego AimTrain, Espero que lo disfrute :-)
+
+
+
+
+import pygame
+
+# Dimensiones de la pantalla
+ANCHO = 800
+ALTO = 600
+# Colores
+BLANCO = (255,255,255)  # R,G,B en el rango [0,255]
+VERDE_BANDERA = (0, 122, 0)
+ROJO = (255, 0, 0)
+
+def dibujar():
+    # Ejemplo del uso de pygame
+    pygame.init()   # Inicializa pygame
+    ventana = pygame.display.set_mode((ANCHO, ALTO))    # Crea la ventana de dibujo
+    reloj = pygame.time.Clock() # Para limitar los fps
+    termina = False # Bandera para saber si termina la ejecución
+
+    while not termina:
+        # Procesa los eventos que recibe
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:  # El usuario hizo click en el botón de salir
+                termina = True
+
+        # Borrar pantalla
+        ventana.fill(BLANCO)
+
+        # Dibujar, aquí haces todos los trazos que requieras
+        pygame.draw.rect(ventana, VERDE_BANDERA, (30, 30, ANCHO-60, ALTO-60), 5)
+        pygame.draw.circle(ventana, ROJO, (ANCHO//2, ALTO//2), 200, 2)
+
+        pygame.display.flip()   # Actualiza trazos
+        reloj.tick(40)          # 40 fps
+
+    pygame.quit()   # termina pygame
+
+
+def main():
+    dibujar()
+
+
+main()
