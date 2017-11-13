0a94c0940b201de6bb0aa02694bfac9ae74d6041
A01376228 <sebfut@hotmail.com>
2017-11-13 12:24:49 -0600

    AimTrain 20: ahora con botones

--- AimTrain.py
+++ AimTrain.py
@@ -15,6 +15,34 @@ BLANCO = (255,255,255)  # R,G,B en el rango [0,255]
 VERDE_BANDERA = (0, 122, 0)
 ROJO = (255, 0, 0)
 
+def dibujarJuego(ventana, btnJugar, lista):
+    btnJugar.rect.left = ANCHO - btnJugar.rect.width
+    btnJugar.rect.top = ALTO - btnJugar.rect.height
+    ventana.blit(btnJugar.image, btnJugar.rect)
+    #Dibujar todos los enemigos
+    for enemigo in lista:
+        ventana.blit(enemigo.image, enemigo.rect)
+
+
+def dibujarMenu(ventana, btnJugar):
+    ventana.blit(btnJugar.image, btnJugar.rect)
+
+
+def generarEnemigos(listaEnemigos, imgBotonPlay):
+    for x in range(3):
+        for y in range(3):
+            cx = 50+ x*260
+            cy = 100 + y*100
+            nuevo = pygame.sprite.Sprite()
+            nuevo.image = imgBotonPlay
+            nuevo.rect = imgBotonPlay.get_rect
+            nuevo.rect.left = cx
+            nuevo.rect.top = cy
+            listaEnemigos.append(nuevo)
+
+
+
+
 def dibujar():
     # Ejemplo del uso de pygame
     pygame.init()   # Inicializa pygame
@@ -22,19 +50,64 @@ def dibujar():
     reloj = pygame.time.Clock() # Para limitar los fps
     termina = False # Bandera para saber si termina la ejecución
 
+    #Estados
+    estado= "menu" #jugando, termina
+
+    #botones
+    imgBotonPlay = pygame.image.load("botonPlay.png")
+    btnJugar =pygame.sprite.Sprite() #SPRITE
+    btnJugar.image = imgBotonPlay
+    btnJugar.rect = imgBotonPlay.get_rect(center = (ANCHO//2, ALTO//2))
+
+    #enemigos
+    listaEnemigos= []
+    generarEnemigos(listaEnemigos, imgBotonPlay)
+
+    #balas
+    listaBalas = []
+
+
+    timer = 0
     while not termina:
         # Procesa los eventos que recibe
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:  # El usuario hizo click en el botón de salir
                 termina = True
+            elif evento.type == pygame.MOUSEBUTTONDOWN:
+                xm, ym = pygame.mouse.get_pos()
+                if estado == "menu":
+
+                    xb, yb, anchoB, altoB = btnJugar.rect
+                    if xm>=xb and xm <=xb + anchoB:
+                        if ym >= yb and ym <= yb + altoB:
+                            estado = "jugando"
+                elif estado == "jugando":
+                    nuevo= pygame.sprite.Sprite()
+                    nuevo.image= imgBotonPlay
+                    nuevo.rect = imgBotonPlay.get_rect
+                    nuevo.rect.left = xm
+                    nuevo.rect.top= ym
+
+                    listaEnemigos.append(nuevo)
+
 
         # Borrar pantalla
         ventana.fill(BLANCO)
 
+        timer += 1/40
+        if timer > 2:
+            #acciones
+            timer = 0
+
         # Dibujar, aquí haces todos los trazos que requieras
-        pygame.draw.rect(ventana, VERDE_BANDERA, (30, 30, ANCHO-60, ALTO-60), 5)
-        pygame.draw.circle(ventana, ROJO, (ANCHO//2, ALTO//2), 200, 2)
-        pygame.draw.rect(ventana, ROJO, (ANCHO//2, ALTO//2 , 20, 20), 20)
+        if estado == "menu":
+            dibujarMenu(ventana, btnJugar)
+        elif estado == "jugando":
+            dibujarJuego(ventana, btnJugar, listaEnemigos)
+
+
+
+
 
         pygame.display.flip()   # Actualiza trazos
         reloj.tick(40)          # 40 fps
